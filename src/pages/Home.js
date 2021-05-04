d8d1688c572a2562e68c259e4cec8d3245ee95c6
jorge <georgellanque@gmail.com>
2021-05-04 18:55:23 -0400

    Adding particle.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,31 +1,33 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-
+import ParticleComponent from "../components/ParticleComponent";
 import './styles/Home.css';
 import logo from '../images/logo.png';
+
 
 export default class Home extends Component {
   render() {
     return (
       <div className="Home">
-        <div className="container">
-          <div className="row">
-            <div className="Home__col col-12 col-md-4">
-              <h1>Badge Management System</h1>
-              <Link className="btn btn-primary" to="/badges">
-                Start
-              </Link>
-            </div>
+        <ParticleComponent className="Home__ParticleComponent" />
+          <div className="container">
+            <div className="row">
+              <div className="Home__col col-12 col-md-4">
+                <h1>Badge Management System</h1>
+                <Link className="btn btn-primary" to="/badges">
+                  Start
+                </Link>
+              </div>
 
-            <div className="Home__col d-none d-md-block col-md-8">
-              <img
-                src={logo}
-                alt="Astronauts"
-                className="img-fluid p-4"
-              />
+              <div className="Home__col d-none d-md-block col-md-8">
+                <img
+                  src={logo}
+                  alt="Astronauts"
+                  className="img-fluid p-4"
+                />
+              </div>
             </div>
           </div>
-        </div>
       </div>
     );
   }
